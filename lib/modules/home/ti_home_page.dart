d1dd043557742cf9f0368c25c7ff52d0dfac62cc
TI_02 <airton.araujo@acad.ifma.edu.br>
2021-09-17 10:27:56 -0300

    home to TI finalized

--- lib/modules/home/ti_home_page.dart
+++ lib/modules/home/ti_home_page.dart
@@ -30,7 +30,7 @@ class _HomePageState extends State<HomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Panorama",
+                    "Relação de Chamados",
                     style: TextStyle(
                         color: Colors.white,
                         fontFamily: "Roboto",
@@ -44,34 +44,154 @@ class _HomePageState extends State<HomePage> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
                         height: 100,
                         width: MediaQuery.of(context).size.width * 0.42,
                         decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(15)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  child:
+                                      Center(child: Icon(Icons.task_rounded)),
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Color.fromRGBO(240, 235, 248, 1)),
+                                ),
+                                Text(
+                                  "21",
+                                  style: TextStyle(
+                                      // color: Colors.white,
+                                      fontFamily: "Roboto",
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            ),
+                            Text("Mês anterior",
+                                style: TextStyle(
+                                    // color: Colors.white,
+                                    fontFamily: "Roboto",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600)),
+                          ],
+                        ),
                       ),
                       Container(
                         height: 100,
+                        padding: EdgeInsets.symmetric(horizontal: 16),
                         width: MediaQuery.of(context).size.width * 0.42,
                         decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(15)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  child:
+                                      Center(child: Icon(Icons.task_rounded)),
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Color.fromRGBO(240, 235, 248, 1)),
+                                ),
+                                Text(
+                                  "15",
+                                  style: TextStyle(
+                                      // color: Colors.white,
+                                      fontFamily: "Roboto",
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w600),
+                                )
+                              ],
+                            ),
+                            Text("Mês Atual",
+                                style: TextStyle(
+                                    // color: Colors.white,
+                                    fontFamily: "Roboto",
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600)),
+                          ],
+                        ),
                       )
                     ],
                   )
                 ],
               ),
             ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.26,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Text(
+                  "Últimos Chamados",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: "Roboto",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ),
             Align(
                 alignment: Alignment.bottomCenter,
                 // top: hei,
                 child: Container(
+                  padding: EdgeInsets.all(8.0),
                   decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.only(
                           topLeft: Radius.circular(25),
                           topRight: Radius.circular(25))),
                   height: MediaQuery.of(context).size.height * 0.6,
+                  child: ListView(
+                    children: [
+                      Card(
+                        child: ListTile(
+                          leading: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(240, 235, 248, 1),
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Icon(Icons.list),
+                          ),
+                          title: Text("Recarga de tonner impressora Brother"),
+                          subtitle: Text("Indústria"),
+                        ),
+                      ),
+                      Card(
+                        child: ListTile(
+                          leading: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(240, 235, 248, 1),
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Icon(Icons.list),
+                          ),
+                          title: Text(
+                            "Relatório de pedidos com erro ortográfico",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          subtitle: Text("Faturamento"),
+                        ),
+                      )
+                    ],
+                  ),
                 ))
           ],
         ),
